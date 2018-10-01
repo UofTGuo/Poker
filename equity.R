b712c2e8eeacb9de6eb30843767c3007a9d45ed8
Zhaoyu Guo <zhaoyuguo@Zhaoyus-MacBook-Pro.local>
2018-09-30 21:04:54 -0400

    updated equity

--- equity.R
+++ equity.R
@@ -4,20 +4,46 @@ library("holdem")
 luck_equity = function(numattable1, playerseats1, chips1, blinds1, dealer1, chipstart1, decision1){
   
   b3 = deal1(numattable1)
+  player1cards = b3$plnum1[1,]
+  player2cards = b3$plnum1[2,]
+  player1suits = b3$plsuit1[1,]
+  player2suits = b3$plsuit1[2,]
+  
   b4 = bid1(numattable1,playerseats1, chips1, blinds1, dealer1, b3, ntable1, decision1) 
   b5 = bid2(numattable1,playerseats1, blinds1, dealer1, b3,b4,2, ntable1, decision1) 
   b6 = bid2(numattable1,playerseats1, blinds1, dealer1, b3,b5,3, ntable1, decision1) 
   b7 = bid2(numattable1,playerseats1, blinds1, dealer1, b3,b6,4, ntable1, decision1) 
   
   # pre-flop equity
+  b4 = bid1(numattable1,playerseats1, chips1, blinds1, dealer1, b3, ntable1, decision1) 
+  pre_flop_win_prob = win_prob(c(),c(),player1cards,player1suits,player2cards,player2suits)
+  
   
   # the flop equity
+  if(b4$all1 == 2){
+    return()
+  }
+  
+  b5 = bid2(numattable1,playerseats1, blinds1, dealer1, b3,b4,2, ntable1, decision1) 
+  flop_win_prob = win_prob(b3$brdnum1[1:3],b3$brdsuit1[1:3],player1cards,player1suits,player2cards,player2suits)
   
   # the turn equity
+  if(b5$all1 == 2){
+    return()
+  }
+  
+  b6 = bid2(numattable1,playerseats1, blinds1, dealer1, b3,b5,3, ntable1, decision1)
+  turn_win_prob = win_prob(b3$brdnum1[1:4],b3$brdsuit1[1:4],player1cards,player1suits,player2cards,player2suits)
   
   # the river equity
+  if(b6$all1 == 2){
+    return()
+  }
   
-  chips2 = calcwin1(numattable1,playerseats1, b3, b7)
+  b7 = bid2(numattable1,playerseats1, blinds1, dealer1, b3,b6,4, ntable1, decision1)
+  river_win_prob = win_prob(b3$brdnum1,b3$brdsuit1,player1cards,player1suits,player2cards,player2suits)
+  
+  chips2 = calcwin1(numattable1,playerseats1, b3, b4)
   }
 
 
