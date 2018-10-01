475c9ab3c164aad82172390735ad40e97dadd839
Zhaoyu Guo <zhaoyuguo@Zhaoyus-MacBook-Pro.local>
2018-09-30 22:20:40 -0400

    updated equity

--- equity.R
+++ equity.R
@@ -1,10 +1,12 @@
 library("holdem")
 
-#luck equity
-luck_equity = function(numattable1, playerseats1, chips1, blinds1, dealer1, chipstart1, decision1){
+#equity
+equity = function(numattable1, playerseats1, chips1, blinds1, dealer1, chipstart1, decision1){
   
-  luck_equity = 0
-  skill_equity = 0
+  p1_luck_equity = 0
+  p2_luck_equity = 0
+  p1_skill_equity = 0
+  p2_skill_equity = 0
   b3 = deal1(numattable1)
   player1cards = b3$plnum1[1,]
   player2cards = b3$plnum1[2,]
@@ -14,34 +16,59 @@ luck_equity = function(numattable1, playerseats1, chips1, blinds1, dealer1, chip
   # pre-flop equity
   b4 = bid1(numattable1,playerseats1, chips1, blinds1, dealer1, b3, ntable1, decision1) 
   pre_flop_win_prob = win_prob(c(),c(),player1cards,player1suits,player2cards,player2suits)
-  pre_flop_chip = calcwin1(numattable1,playerseats1, b3, b4)
+  if(b4$rb[1,1] == 20 && b4$rb[2,1] == 10){
+    p1_luck_equity =+ pre_flop_win_prob[1]*(b4$rb[1,1]+b4$rb[2,1]) - b4$rb[1,1]
+    p2_luck_equity =+ pre_flop_win_prob[2]*(b4$rb[1,1]+b4$rb[2,1]) - b4$rb[2,1]
+    }
+  else{
+    p1_luck_equity =+ pre_flop_win_prob[1]*(2*blinds1[2]) - blinds1[2]
+    p2_luck_equity =+ pre_flop_win_prob[2]*(2*blinds1[2]) - blinds1[2]
+    p1_skill_equity =+ pre_flop_win_prob[1]*(b4$p1-2*blinds1[2]) - (b4$rb[1,1] - blinds1[2])
+    p2_skill_equity =+ pre_flop_win_prob[2]*(b4$p1-2*blinds1[2]) - (b4$rb[2,1] - blinds1[2])
+    }
+  #pre_flop_chip = calcwin1(numattable1,playerseats1, b3, b4)
   
   # the flop equity
   if(b4$all1 == 2){
-    return(c(luck_equity,skill_equity))
+    return(c(p1_luck_equity,p2_luck_equity,p1_skill_equity,p2_skill_equity))
+    break
   }
   b5 = bid2(numattable1,playerseats1, blinds1, dealer1, b3,b4,2, ntable1, decision1) 
   flop_win_prob = win_prob(b3$brdnum1[1:3],b3$brdsuit1[1:3],player1cards,player1suits,player2cards,player2suits)
-  flop_chip = calcwin1(numattable1,playerseats1, b3, b5)
+  p1_luck_equity =+ (flop_win_prob[1]-pre_flop_win_prob[1])*b4$p1
+  p2_luck_equity =+ (flop_win_prob[2]-pre_flop_win_prob[2])*b4$p1
+  p1_skill_equity =+ (flop_win_prob[1])*(b5$p1-b4$p1) - b5$rb[1,2]
+  p2_skill_equity =+ (flop_win_prob[2])*(b5$p1-b4$p1) - b5$rb[2,2]
+  #flop_chip = calcwin1(numattable1,playerseats1, b3, b5)
   
   # the turn equity
   if(b5$all1 == 2){
-    return(c(luck_equity,skill_equity))
+    return(c(p1_luck_equity,p2_luck_equity,p1_skill_equity,p2_skill_equity))
+    break
   }
   b6 = bid2(numattable1,playerseats1, blinds1, dealer1, b3,b5,3, ntable1, decision1)
   turn_win_prob = win_prob(b3$brdnum1[1:4],b3$brdsuit1[1:4],player1cards,player1suits,player2cards,player2suits)
-  turn_chip = calcwin1(numattable1,playerseats1, b3, b6)
+  p1_luck_equity =+ (turn_win_prob[1]-pre_flop_win_prob[1])*b5$p1
+  p2_luck_equity =+ (turn_win_prob[2]-pre_flop_win_prob[2])*b5$p1
+  p1_skill_equity =+ (turn_win_prob[1])*(b6$p1-b5$p1) - b6$rb[1,3]
+  p2_skill_equity =+ (turn_win_prob[2])*(b6$p1-b5$p1) - b6$rb[2,3]
+  #turn_chip = calcwin1(numattable1,playerseats1, b3, b6)
   
   # the river equity
   if(b6$all1 == 2){
-    return(c(luck_equity,skill_equity))
+    return(c(p1_luck_equity,p2_luck_equity,p1_skill_equity,p2_skill_equity))
+    break
   }
   b7 = bid2(numattable1,playerseats1, blinds1, dealer1, b3,b6,4, ntable1, decision1)
   river_win_prob = win_prob(b3$brdnum1,b3$brdsuit1,player1cards,player1suits,player2cards,player2suits)
-  river_chip = calcwin1(numattable1,playerseats1, b3, b7)
+  p1_luck_equity =+ (river_win_prob[1]-pre_flop_win_prob[1])*b6$p1
+  p2_luck_equity =+ (river_win_prob[2]-pre_flop_win_prob[2])*b6$p1
+  p1_skill_equity =+ (river_win_prob[1])*(b7$p1-b6$p1) - b7$rb[1,3]
+  p2_skill_equity =+ (river_win_prob[2])*(b7$p1-b6$p1) - b7$rb[2,3]
+  #river_chip = calcwin1(numattable1,playerseats1, b3, b7)
   
-  return(c(luck_equity,skill_equity))
-  }
+  return(c(p1_luck_equity,p2_luck_equity,p1_skill_equity,p2_skill_equity))
+}
 
 #helper function - winning probability in each betting round
 win_prob = function(boardcards,boardsuits,player1cards,player1suits,player2cards,player2suits){
@@ -54,16 +81,19 @@ win_prob = function(boardcards,boardsuits,player1cards,player1suits,player2cards
 }
 
 # Example
+
 numattable1 = 2
 playerseats1 = c(2,1)
 chips1 = c(100,100)
 blinds1 = c(10,20)
 dealer1 = 1
-ntable1 = 1
-myfast1 = 2
-t1 = 0.5
-t2 = 1
 chipstart1 = 100
-decision1 = list(gravity, tommy)
+decision1 = list(gravity, tommy) # change to other type of players
+
+result = matrix(, nrow = 100, ncol = 4)
+for(i in 1:100){
+  result[i,] = equity(numattable1, playerseats1, chips1, blinds1, dealer1, chipstart1, decision1)
+}
+
 
 
