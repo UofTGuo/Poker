ab9a11f0a0f3de5d18d4efe5929a830089e48879
Zhaoyu Guo <zhaoyuguo@Zhaoyus-MacBook-Pro.local>
2018-10-09 13:51:24 -0400

    updated

--- deepstack_data_clean.R
+++ deepstack_data_clean.R
@@ -1,4 +1,14 @@
+setwd("/Users/zhaoyuguo/Desktop/Poker Project/new/Poker")
 library(holdem)
+source("convert_functions.R")
 library(dplyr)
 
-all_hands <- read.csv("~/Desktop/Poker Project/new/Poker/data/all_hands.csv")
+#Reference data set, containing some important informations
+all_hands = read.csv("~/Desktop/Poker Project/new/Poker/data/all_hands.csv")
+
+ds_data = read.csv("~/Desktop/Poker Project/new/Poker/data/all_hands2.csv")
+names(ds_data) = as.matrix(ds_data[1,])
+#remove first row
+ds_data = ds_data[-1,]
+
+#Implement betting functions
--- luck_vs_skill.R
+++ luck_vs_skill.R
@@ -1,3 +1,7 @@
+setwd("/Users/zhaoyuguo/Desktop/Poker Project/new/Poker")
+source("convert_functions.R")
+source("deepstack_data_clean.R")
+source("equity.R")
 library(holdem)
 
 #DeepStack 
