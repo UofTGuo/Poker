adf523d080c7e46343f580a1b5b02bafdef95691
irwinkhuu <43473195+irwinkhuu@users.noreply.github.com>
2018-11-17 17:02:09 -0500

    Add files via upload
    confirmed for all cases pre-flop

--- equity.R
+++ equity.R
@@ -5,11 +5,6 @@ options(digits=6)
 # calculating equity due to luck and equity due to skill
 equity = function(numattable1, playerseats1, chips1, blinds1, dealer1, chipstart1, decision1, iters){
   
-  p1_luck_equity = 0
-  p2_luck_equity = 0
-  p1_skill_equity = 0
-  p2_skill_equity = 0
-  
   dealt_index = sample_cards(numattable1)
   player_index = dealt_index[1:(2*numattable1)]
   player_info = switch2(player_index)
@@ -31,66 +26,86 @@ equity = function(numattable1, playerseats1, chips1, blinds1, dealer1, chipstart
   b3$brdsuit1 = board_info$st
   
   # Pre-flop equity
-  b4 = bid1(numattable1,playerseats1, chips1, blinds1, dealer1, b3, ntable1, decision1) 
+
+  p1_luck_equity = 0
+  p2_luck_equity = 0
+  p1_skill_equity = 0
+  p2_skill_equity = 0
+  
+  b4 = bid1(numattable1,playerseats1, chips1, blinds1, dealer1, b3,, decision1) 
   pre_flop_win_prob = win_prob(numattable1,dealt_index,"pre_flop", iters)
   
   # case of small blind directly folding
-  if(b4$rb[1,1] == 100 && b4$rb[2,1] == 50){
-    
-    #New Proposed way
-    #p1_luck_equity = p1_luck_equity + min((2*b4$rb[1,1]*pre_flop_win_prob[1]-b4$rb[1,1]),b4$rb[2,1])
-    #p2_luck_equity = p2_luck_equity + (2*b4$rb[1,1]*pre_flop_win_prob[2] - rb[1,1])
-    #p1_skill_equity = p1_skill_equity + b4$rb[2,1] - p1_luck_equity
-    #p2_skill_equity = p2_skill_equity - b4$rb[2,1] - p2_luck_equity
-    
-    #Schoenberg's way
-    p1_luck_equity = p1_luck_equity + (2*b4$rb[1,1]*pre_flop_win_prob[1] - b4$rb[1,1])
-    p2_luck_equity = p2_luck_equity + max((2*b4$rb[1,1]*pre_flop_win_prob[2]-b4$rb[1,1]),-b4$rb[2,1])
-    p1_skill_equity = p1_skill_equity + b4$rb[2,1] - p1_luck_equity
-    p2_skill_equity = p2_skill_equity - b4$rb[2,1] - p2_luck_equity
-    
-    return(c(p1_luck_equity,p2_luck_equity,p1_skill_equity,p2_skill_equity))
-  }
-  
-  # case of small blind calling the big blind or raising a blind
-  p1_luck_equity = p1_luck_equity + pre_flop_win_prob[1]*(2*blinds1[2]) - blinds1[2]
-  p2_luck_equity = p2_luck_equity + pre_flop_win_prob[2]*(2*blinds1[2]) - blinds1[2]
-  p1_skill_equity = p1_skill_equity + pre_flop_win_prob[1]*(b4$p1-2*blinds1[2]) - (b4$rb[1,1] - blinds1[2])
-  p2_skill_equity = p2_skill_equity + pre_flop_win_prob[2]*(b4$p1-2*blinds1[2]) - (b4$rb[2,1] - blinds1[2])
-  
-  # Flop equity
-  if(b4$all1 == 2){
+  if(b4$rb[1,1] == blinds1[2] && b4$rb[2,1] == blinds1[1]){
+
+  #New Proposed or Schoenberg's Way for luck (skill changes depending on this value)
+    p1_luck_equity = p1_luck_equity + min((2*blinds1[2]*pre_flop_win_prob[1]-blinds1[2]),blinds1[1])
+    p2_luck_equity = p2_luck_equity + (2*blinds1[2]*pre_flop_win_prob[2] - blinds1[2])
+  #p1_luck_equity = p1_luck_equity + (2*blinds1[2]*pre_flop_win_prob[1] - blinds1[2])
+  #p2_luck_equity = p2_luck_equity + max((2*blinds1[2]*pre_flop_win_prob[2]-blinds1[2]),-blinds1[1])
+    p1_skill_equity = p1_skill_equity + blinds1[1] - p1_luck_equity
+    p2_skill_equity = p2_skill_equity - blinds1[1] - p2_luck_equity
     return(c(p1_luck_equity,p2_luck_equity,p1_skill_equity,p2_skill_equity))
     break
+
+  } else {
+  #New Proposed or Schoenberg's Way for luck (skill changes depending on this value)
+    p1_luck_equity = p1_luck_equity + (2*blinds1[2]*pre_flop_win_prob[1] - blinds1[2])
+    p2_luck_equity = p2_luck_equity + max((2*blinds1[2]*pre_flop_win_prob[2]-blinds1[2]),-blinds1[1])
+  #p1_luck_equity = p1_luck_equity + (2*blinds1[2]*pre_flop_win_prob[1] - blinds1[2])
+  #p2_luck_equity = p2_luck_equity + max((2*blinds1[2]*pre_flop_win_prob[2]-blinds1[2]),-blinds1[1])
+  }
+
+  if(b4$all1 == 2){
+  #case of small blind matching blinds then folding
+    if(b4$rb[1,1] > b4$rb[2,1]){
+
+      p1_skill_equity = p1_skill_equity + 1*(b4$p1) - b4$rb[1,1] - p1_luck_equity
+      p2_skill_equity = p2_skill_equity + 0*(b4$p1) - b4$rb[2,1] - p2_luck_equity
+      return(c(p1_luck_equity,p2_luck_equity,p1_skill_equity,p2_skill_equity))
+      break
+
+    } else {
+
+   #case of small blind raising and big blind folding
+      p1_skill_equity = p1_skill_equity + 0*(b4$p1) - b4$rb[1,1] - p1_luck_equity
+      p2_skill_equity = p2_skill_equity + 1*(b4$p1) - b4$rb[2,1] - p2_luck_equity
+      return(c(p1_luck_equity,p2_luck_equity,p1_skill_equity,p2_skill_equity))
+      break
+      }
   }
   
-  b5 = bid2(numattable1,playerseats1, blinds1, dealer1, b3,b4,2, ntable1, decision1) 
+  #case where no one folds
+  p1_skill_equity = p1_skill_equity + pre_flop_win_prob[1]*(b4$p1) - b4$rb[1,1] - p1_luck_equity
+  p2_skill_equity = p2_skill_equity + pre_flop_win_prob[2]*(b4$p1) - b4$rb[2,1] - p2_luck_equity
+
+  # Flop equity  
+  b5 = bid2(numattable1,playerseats1, blinds1, dealer1, b3,b4,2,, decision1) 
   flop_win_prob = win_prob(numattable1,dealt_index,"flop", iters)
   p1_luck_equity = p1_luck_equity + (flop_win_prob[1]-pre_flop_win_prob[1])*b4$p1
   p2_luck_equity = p2_luck_equity + (flop_win_prob[2]-pre_flop_win_prob[2])*b4$p1
   p1_skill_equity = p1_skill_equity + (flop_win_prob[1])*(b5$p1-b4$p1) - b5$rb[1,2]
   p2_skill_equity = p2_skill_equity + (flop_win_prob[2])*(b5$p1-b4$p1) - b5$rb[2,2]
-  
-  # Turn equity
+
   if(b5$all1 == 2){
     return(c(p1_luck_equity,p2_luck_equity,p1_skill_equity,p2_skill_equity))
     break
   }
   
-  b6 = bid2(numattable1,playerseats1, blinds1, dealer1, b3, b5, 3, ntable1, decision1)
+  # Turn equity
+  b6 = bid2(numattable1,playerseats1, blinds1, dealer1, b3, b5, 3,, decision1)
   turn_win_prob = win_prob(numattable1, dealt_index, "turn", iters)
   p1_luck_equity = p1_luck_equity + (turn_win_prob[1]-pre_flop_win_prob[1])*b5$p1
   p2_luck_equity = p2_luck_equity + (turn_win_prob[2]-pre_flop_win_prob[2])*b5$p1
   p1_skill_equity = p1_skill_equity + (turn_win_prob[1])*(b6$p1-b5$p1) - b6$rb[1,3]
   p2_skill_equity = p2_skill_equity + (turn_win_prob[2])*(b6$p1-b5$p1) - b6$rb[2,3]
-  
-  # River equity
   if(b6$all1 == 2){
     return(c(p1_luck_equity,p2_luck_equity,p1_skill_equity,p2_skill_equity))
     break
   }
-  
-  b7 = bid2(numattable1, playerseats1, blinds1, dealer1, b3, b6, 4, ntable1, decision1)
+ 
+  # River equity
+  b7 = bid2(numattable1, playerseats1, blinds1, dealer1, b3, b6, 4,, decision1)
   river_win_prob = win_prob(numattable1, dealt_index,"river", iters)
   p1_luck_equity = p1_luck_equity + (river_win_prob[1]-pre_flop_win_prob[1])*b6$p1
   p2_luck_equity = p2_luck_equity + (river_win_prob[2]-pre_flop_win_prob[2])*b6$p1
@@ -102,14 +117,14 @@ equity = function(numattable1, playerseats1, chips1, blinds1, dealer1, chipstart
 
 # Sampling index of cards
 sample_cards = function(numattable1){
-  all_cards = order(runif(52))
+  all_cards = sample(52)
   index = sample(52,size=numattable1*2+5,replace=FALSE)
   return(index)
 }
 
 # Calculate winning probability using Monte Carlo method and exact method.
 win_prob = function(numattable1,dealt_index, round, iters){
-  all_index = sample(1:52)
+  all_index = sample(52)
   pre_flop_index = dealt_index[1:(2*numattable1)]
   flop_index = dealt_index[1:(2*numattable1+3)]
   turn_index = dealt_index[1:(2*numattable1+4)]
@@ -146,9 +161,9 @@ win_prob = function(numattable1,dealt_index, round, iters){
 	  tie = tie + 1
 	  }
      }
-    winprob[1] <- temp/iters
     winprob[3] <- tie/iters
-    winprob[2] <- 1-winprob[1]-winprob[3]
+    winprob[1] <- temp/iters+winprob[3]/2
+    winprob[2] <- 1-winprob[1]
     return(winprob)
     break
   }
@@ -171,9 +186,9 @@ win_prob = function(numattable1,dealt_index, round, iters){
 	  tie = tie + 1
 	}
     }
-    winprob[1] <- temp/flop_comb
     winprob[3] <- tie/flop_comb
-    winprob[2] <- 1-winprob[1]-winprob[3]
+    winprob[1] <- temp/flop_comb+winprob[3]/2
+    winprob[2] <- 1-winprob[1]
     return(winprob)
     break
   }
@@ -195,9 +210,9 @@ win_prob = function(numattable1,dealt_index, round, iters){
 	  tie <<- tie + 1
 	}
     }
-    winprob[1] <- temp/turn_comb
     winprob[3] <- tie/turn_comb
-    winprob[2] <- 1-winprob[1]-winprob[3]
+    winprob[1] <- temp/turn_comb+winprob[3]/2
+    winprob[2] <- 1-winprob[1]
     return(winprob)
     break
   }
@@ -224,22 +239,22 @@ win_prob = function(numattable1,dealt_index, round, iters){
 }
 
 # Calculating average equity
-avg_equity = function(numattable, playerseats, chips, blinds, dealer, chipstart, decision, num_hand, iters){
-  p1_luck = p2_luck = p1_skill = p2_skill = p1_chip = p2_chip = c()
+avg_equity = function(numattable, chips, blinds, dealer, chipstart, decision, num_hand, iters){
+  p1_luck = p2_luck = p1_skill = p2_skill = p1_chip = p2_chip = numeric(num_hand)
+  cond <- ((1:num_hand)%%2) == 0
   for(i in 1:num_hand){
-    if((i%%2) == 0){
+    if(cond[i]){
       temp = equity(numattable, c(1,2), chips, blinds, dealer, chipstart, decision,iters)
-    }
-    if((i%%2) == 1){
+    } else {
       temp = equity(numattable, c(2,1), chips, blinds, dealer, chipstart, decision,iters)
     }
     cat(c(temp,(chips[1]+temp[1]+temp[3]),(chips[2]+temp[2]+temp[4])),"\n")
-    p1_luck = c(p1_luck, temp[1])
-    p2_luck = c(p2_luck, temp[2])
-    p1_skill = c(p1_skill, temp[3])
-    p2_skill = c(p2_skill, temp[4])
-    p1_chip = c(p1_chip,(chips[1]+temp[1]+temp[3]))
-    p2_chip = c(p2_chip,(chips[2]+temp[2]+temp[4]))
+    p1_luck[i] = temp[1]
+    p2_luck[i] = temp[2]
+    p1_skill[i] = temp[3]
+    p2_skill[i] = temp[4]
+    p1_chip[i] = chips[1]+temp[1]+temp[3]
+    p2_chip[i] = chips[2]+temp[2]+temp[4]
   }
   cat("final output", c(mean(p1_luck),mean(p2_luck),mean(p1_skill),mean(p2_skill),mean(p1_chip),mean(p2_chip)),"\n")
   output = c(mean(p1_luck),mean(p2_luck),mean(p1_skill),mean(p2_skill),mean(p1_chip),mean(p2_chip))
@@ -269,7 +284,7 @@ M=10
 
 dec1_result_list = matrix(nrow=M,ncol=6)
 for(i in 1:M){
- dec1_result_list[i,] = avg_equity(numattable1,playerseats1,chips1,blinds1,dealer1,chipstart1,decision_one,num_hand,iters)
+ dec1_result_list[i,] = avg_equity(numattable1,chips1,blinds1,dealer1,chipstart1,decision_one,num_hand,iters)
 }
 mean(dec1_result_list[,1])
 mean(dec1_result_list[,2])
@@ -286,7 +301,7 @@ sd(dec1_result_list[,6])/sqrt(M)
 
 dec2_result_list = matrix(nrow=M,ncol=6)
 for(i in 1:M){
-  dec2_result_list[i,] = avg_equity(numattable1,playerseats1,chips1,blinds1,dealer1,chipstart1,decision_two,num_hand,iters)
+  dec2_result_list[i,] = avg_equity(numattable1,chips1,blinds1,dealer1,chipstart1,decision_two,num_hand,iters)
 }
 mean(dec2_result_list[,1])
 mean(dec2_result_list[,2])
@@ -303,7 +318,7 @@ sd(dec2_result_list[,6])/sqrt(M)
 
 dec3_result_list = matrix(nrow=M,ncol=6)
 for(i in 1:M){
-  dec3_result_list[i,] = avg_equity(numattable1,playerseats1,chips1,blinds1,dealer1,chipstart1,decision_three,num_hand,iters)
+  dec3_result_list[i,] = avg_equity(numattable1,chips1,blinds1,dealer1,chipstart1,decision_three,num_hand,iters)
 }
 mean(dec3_result_list[,1])
 mean(dec3_result_list[,2])
@@ -320,7 +335,7 @@ sd(dec3_result_list[,6])/sqrt(M)
 
 dec4_result_list = matrix(nrow=M,ncol=6)
 for(i in 1:M){
-  dec4_result_list[i,] = avg_equity(numattable1,playerseats1,chips1,blinds1,dealer1,chipstart1,decision_four,num_hand,iters)
+  dec4_result_list[i,] = avg_equity(numattable1,chips1,blinds1,dealer1,chipstart1,decision_four,num_hand,iters)
 }
 mean(dec4_result_list[,1])
 mean(dec4_result_list[,2])
@@ -337,7 +352,7 @@ sd(dec4_result_list[,6])/sqrt(M)
 
 dec5_result_list = matrix(nrow=M,ncol=6)
 for(i in 1:M){
-  dec5_result_list[i,] = avg_equity(numattable1,playerseats1,chips1,blinds1,dealer1,chipstart1,decision_five,num_hand,iters)
+  dec5_result_list[i,] = avg_equity(numattable1,chips1,blinds1,dealer1,chipstart1,decision_five,num_hand,iters)
 }
 mean(dec5_result_list[,1])
 mean(dec5_result_list[,2])
@@ -354,7 +369,7 @@ sd(dec5_result_list[,6])/sqrt(M)
 
 dec6_result_list = matrix(nrow=M,ncol=6)
 for(i in 1:M){
-  dec6_result_list[i,] = avg_equity(numattable1,playerseats1,chips1,blinds1,dealer1,chipstart1,decision_six,num_hand,iters)
+  dec6_result_list[i,] = avg_equity(numattable1,chips1,blinds1,dealer1,chipstart1,decision_six,num_hand,iters)
 }
 mean(dec6_result_list[,1])
 mean(dec6_result_list[,2])
