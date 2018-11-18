874c2b2cfd8b6313e4d2c4d3fbc716ba6c64230c
irwinkhuu <43473195+irwinkhuu@users.noreply.github.com>
2018-11-18 04:03:47 -0500

    Add files via upload

--- equity.R
+++ equity.R
@@ -309,16 +309,22 @@ avg_equity = function(numattable, chips, blinds, dealer, chipstart, decision, nu
   for(i in 1:num_hand){
     if(cond[i]){
       temp = equity(numattable, c(1,2), chips, blinds, dealer, chipstart, decision,iters)
+      p1_luck[i] = temp[1]
+      p2_luck[i] = temp[2]
+      p1_skill[i] = temp[3]
+      p2_skill[i] = temp[4]
+      p1_chip[i] = chips[1]+temp[1]+temp[3]
+      p2_chip[i] = chips[2]+temp[2]+temp[4]
     } else {
       temp = equity(numattable, c(2,1), chips, blinds, dealer, chipstart, decision,iters)
+      p1_luck[i] = temp[2]
+      p2_luck[i] = temp[1]
+      p1_skill[i] = temp[4]
+      p2_skill[i] = temp[2]
+      p1_chip[i] = chips[2]+temp[2]+temp[4]
+      p2_chip[i] = chips[1]+temp[1]+temp[3]
     }
     cat(c(temp,(chips[1]+temp[1]+temp[3]),(chips[2]+temp[2]+temp[4])),"\n")
-    p1_luck[i] = temp[1]
-    p2_luck[i] = temp[2]
-    p1_skill[i] = temp[3]
-    p2_skill[i] = temp[4]
-    p1_chip[i] = chips[1]+temp[1]+temp[3]
-    p2_chip[i] = chips[2]+temp[2]+temp[4]
   }
   cat("final output", c(mean(p1_luck),mean(p2_luck),mean(p1_skill),mean(p2_skill),mean(p1_chip),mean(p2_chip)),"\n")
   output = c(mean(p1_luck),mean(p2_luck),mean(p1_skill),mean(p2_skill),mean(p1_chip),mean(p2_chip))
