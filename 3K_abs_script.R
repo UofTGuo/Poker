10ff4f22f2d8acddf87082fdcb45516cc3573488
Zhaoyu Guo <zhaoyuguo@Zhaoyus-MacBook-Pro.local>
2018-12-31 17:33:22 -0500

    updated

--- 3K_abs_script.R
+++ 3K_abs_script.R
@@ -15,6 +15,7 @@ sd(abs(GoodLoose_vs_BadLoose$p2_skill[1:1500]))/sqrt(1500)
 sd(abs(GoodLoose_vs_BadLoose$p1_chip[1:1500]))/sqrt(1500)
 sd(abs(GoodLoose_vs_BadLoose$p2_chip[1:1500]))/sqrt(1500)
 #Correlations
+cor(cbind(abs(GoodLoose_vs_BadLoose$p1_skill[1:1500]),abs(GoodLoose_vs_BadLoose$p1_luck[1:1500])))
 cor(cbind(abs(GoodLoose_vs_BadLoose$p2_skill[1:1500]),abs(GoodLoose_vs_BadLoose$p2_luck[1:1500])))
 
 # (BadLoose|GoodLoose) BadLoose is big blind, GoodLoose is small blind
@@ -31,8 +32,8 @@ sd(abs(GoodLoose_vs_BadLoose$p1_skill[1501:3000]))/sqrt(1500)
 sd(abs(GoodLoose_vs_BadLoose$p2_chip[1501:3000]))/sqrt(1500)
 sd(abs(GoodLoose_vs_BadLoose$p1_chip[1501:3000]))/sqrt(1500)
 #Correlations
+cor(cbind(abs(GoodLoose_vs_BadLoose$p2_skill[1501:3000]),abs(GoodLoose_vs_BadLoose$p2_luck[1501:3000])))
 cor(cbind(abs(GoodLoose_vs_BadLoose$p1_skill[1501:3000]),abs(GoodLoose_vs_BadLoose$p1_luck[1501:3000])))
-
 
 #Example of Tommy vs. New players Table(Tommy vs. GoodLoose)
 # (Tommy|GoodLoose) Tommy is big blind, GoodLoose is small blind
@@ -49,6 +50,7 @@ sd(abs(GoodLoose_vs_Tommy$p1_skill[1501:3000]))/sqrt(1500)
 sd(abs(GoodLoose_vs_Tommy$p2_chip[1501:3000]))/sqrt(1500)
 sd(abs(GoodLoose_vs_Tommy$p1_chip[1501:3000]))/sqrt(1500)
 #Correlations
+cor(cbind(abs(GoodLoose_vs_Tommy$p2_skill[1501:3000]),abs(GoodLoose_vs_Tommy$p2_luck[1501:3000])))
 cor(cbind(abs(GoodLoose_vs_Tommy$p1_skill[1501:3000]),abs(GoodLoose_vs_Tommy$p1_luck[1501:3000])))
 
 # (GoodLoose|Tommy) GoodLoose is big blind, Tommy is small blind
@@ -65,8 +67,8 @@ sd(abs(GoodLoose_vs_Tommy$p2_skill[1:1500]))/sqrt(1500)
 sd(abs(GoodLoose_vs_Tommy$p1_chip[1:1500]))/sqrt(1500)
 sd(abs(GoodLoose_vs_Tommy$p2_chip[1:1500]))/sqrt(1500)
 #Correlations
+cor(cbind(abs(GoodLoose_vs_Tommy$p1_skill[1:1500]),abs(GoodLoose_vs_Tommy$p1_luck[1:1500])))
 cor(cbind(abs(GoodLoose_vs_Tommy$p2_skill[1:1500]),abs(GoodLoose_vs_Tommy$p2_luck[1:1500])))
-
 
 #Example of Zelda vs. New players Table(Zelda vs. GoodLoose)
 # (Zelda|GoodLoose) Zelda is big blind, GoodLoose is small blind
@@ -83,6 +85,7 @@ sd(abs(GoodLoose_vs_Zelda$p1_skill[1501:3000]))/sqrt(1500)
 sd(abs(GoodLoose_vs_Zelda$p2_chip[1501:3000]))/sqrt(1500)
 sd(abs(GoodLoose_vs_Zelda$p1_chip[1501:3000]))/sqrt(1500)
 #Correlations
+cor(cbind(abs(GoodLoose_vs_Zelda$p2_skill[1501:3000]),abs(GoodLoose_vs_Zelda$p2_luck[1501:3000])))
 cor(cbind(abs(GoodLoose_vs_Zelda$p1_skill[1501:3000]),abs(GoodLoose_vs_Zelda$p1_luck[1501:3000])))
 
 # (GoodLoose|Zelda) GoodLoose is big blind, Zelda is small blind
@@ -99,4 +102,5 @@ sd(abs(GoodLoose_vs_Zelda$p2_skill[1:1500]))/sqrt(1500)
 sd(abs(GoodLoose_vs_Zelda$p1_chip[1:1500]))/sqrt(1500)
 sd(abs(GoodLoose_vs_Zelda$p2_chip[1:1500]))/sqrt(1500)
 #Correlations
+cor(cbind(abs(GoodLoose_vs_Zelda$p1_skill[1:1500]),abs(GoodLoose_vs_Zelda$p1_luck[1:1500])))
 cor(cbind(abs(GoodLoose_vs_Zelda$p2_skill[1:1500]),abs(GoodLoose_vs_Zelda$p2_luck[1:1500])))
