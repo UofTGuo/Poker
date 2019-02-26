7f882d23a4e443804e23eed8996dde24be73c205
irwinkhuu <43473195+irwinkhuu@users.noreply.github.com>
2019-02-26 17:54:04 -0500

    Add files via upload

---
+++ iterated_average_h.R
@@ -0,0 +1,28 @@
+#Take data from https://github.com/UofTGuo/Poker/tree/master/simulation_result/3K_hands
+data <- read.csv("https://raw.githubusercontent.com/UofTGuo/Poker/master/simulation_result/3K_hands/BadLoose_vs_GoodTight.csv")
+gtbl <- data[1501:3000,]
+
+gt_avg_luck <- bl_avg_luck <- gt_avg_skill <- bl_avg_skill <- numeric(nrow(gtbl))
+bl_se_luck <- gt_se_luck <- bl_se_skill <- gt_se_skill <- rep(0,nrow(gtbl))
+for(i in 1:nrow(gtbl)){
+	bl_avg_luck[i] <- mean(gtbl[1:i,2])
+	gt_avg_luck[i] <- mean(gtbl[1:i,3])
+	bl_avg_skill[i] <- mean(gtbl[1:i,4])
+	gt_avg_skill[i] <- mean(gtbl[1:i,5])
+	if(i >= 2){
+		bl_se_luck[i] <- sd(gtbl[1:i,2])/sqrt(i)
+		gt_se_luck[i] <- sd(gtbl[1:i,3])/sqrt(i)
+		bl_se_skill[i] <- sd(gtbl[1:i,4])/sqrt(i)
+		gt_se_skill[i] <- sd(gtbl[1:i,5])/sqrt(i)
+	}
+}
+
+#Change main="" to show who big blind is
+plot(1:nrow(gtbl), gt_avg_skill, type="l", col="green", lwd=2,
+	xlab="Hand Number", ylab="Average", main="Iterated Average of GoodTight(BB) vs. BadLoose")
+points(1:nrow(gtbl), gt_avg_skill - 1.96*gt_se_skill, type="l", lty=2, col="darkgrey")
+points(1:nrow(gtbl), gt_avg_skill + 1.96*gt_se_skill, type="l", lty=2, col="darkgrey")
+points(1:nrow(gtbl), gt_avg_luck, type="l", col="red", lwd=2)
+points(1:nrow(gtbl), gt_avg_luck - 1.96*gt_se_luck, type="l", lty=2, col="black")
+points(1:nrow(gtbl), gt_avg_luck + 1.96*gt_se_luck, type="l", lty=2, col="black")
+legend("bottomright", legend=c("Skill","SE(Skill)","Luck","SE(Luck)"), col=c("green","darkgrey","red","black"), lty=c(1,2,1,2))
